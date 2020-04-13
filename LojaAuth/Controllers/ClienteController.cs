f49f0a62edeb64d4c97905a426fada6d774d5fd2
icosta7 <icosta@vitait.com>
2020-04-13 17:44:41 -0300

    first commit

---
+++ LojaAuth/Controllers/ClienteController.cs
@@ -0,0 +1,128 @@
+﻿using AutoMapper;
+using IdentityModel.Client;
+using LojaAuth.Api.Models;
+using LojaAuth.DTO;
+using LojaAuth.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LojaAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly IClienteService _clienteService;
+        private readonly IMapper _mapper;
+
+        public ClienteController(IClienteService clienteService, IMapper mapper)
+        {
+            _clienteService = clienteService;
+            _mapper = mapper;
+        }
+
+        // GET api/cliente/{id}
+        [HttpGet("{id}")]
+        public ActionResult<ClienteDTO> Get(int id)
+        {
+            var cliente = _clienteService.ProcurarPorId(id);
+
+            if (cliente != null)
+            {
+                var retorno = _mapper.Map<ClienteDTO>(cliente);
+
+                return Ok(retorno);
+            }
+            else
+                return NotFound();
+        }
+
+        // POST api/cliente
+        // binding argumento
+        [HttpPost]
+        public ActionResult<ClienteDTO> Post([FromBody]ClienteDTO value)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+
+            var endereco = new Endereco()
+            {
+                Logradouro = value.Endereco.Logradouro,
+                Numero = value.Endereco.Numero,
+                Complemento = value.Endereco.Complemento,
+                Bairro = value.Endereco.Bairro,
+                Cidade = value.Endereco.Cidade
+            };
+
+            var cliente = new Cliente()
+            {
+                Nome = value.Nome,
+                EnderecoDeEntrega = endereco
+            };
+
+            var retornoCliente = _clienteService.Salvar(cliente);
+
+            var retorno = _mapper.Map<ClienteDTO>(retornoCliente);
+
+            return Ok(retorno);
+        }
+
+        // POST api/cliente
+        [HttpPut]
+        public ActionResult<ClienteDTO> Put([FromBody] ClienteDTO value)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var endereco = new Endereco()
+            {
+                Id = value.EnderecoId,
+                Logradouro = value.Endereco.Logradouro,
+                Numero = value.Endereco.Numero,
+                Complemento = value.Endereco.Complemento,
+                Bairro = value.Endereco.Bairro,
+                Cidade = value.Endereco.Cidade
+            };
+
+            var cliente = new Cliente()
+            {
+                Id = value.Id,
+                Nome = value.Nome,
+                EnderecoId = value.EnderecoId,
+                EnderecoDeEntrega = endereco
+            };
+
+            var retornoCliente = _clienteService.Salvar(cliente);
+            var retorno = _mapper.Map<ClienteDTO>(retornoCliente);
+
+            return Ok(retorno);
+        }
+
+
+        [HttpGet("getToken")]
+        public async Task<ActionResult<TokenResponse>> GetToken([FromBody]TokenDTO value)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            // async request - await para aguardar retorno
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+
+            // nesta parte, temos um exemplo de requisição com o tipo "password" 
+            // esta é a forma mais comum
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "codenation.api_client", "codenation.api_secret");
+            var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync(value.UserName, value.Password, "codenation");
+
+            // Se não tiver tiver um erro retornar token
+            if (!tokenResponse.IsError)
+            {
+                return Ok(tokenResponse);
+            }
+
+            //retorna não autorizado e descrição do erro
+            return Unauthorized(tokenResponse.ErrorDescription);
+        }
+
+    }
+}
---
+++ LojaAuth/Migrations/20200413142903_passwordCliente.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LojaAuth.Migrations
+{
+    public partial class passwordCliente : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Cliente",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Cliente",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Cliente");
+        }
+    }
+}
